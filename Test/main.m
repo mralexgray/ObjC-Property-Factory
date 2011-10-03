52964c3c1f76bf9e65735a94ba957a5b2adee253
Jonathan Wight <jwight@mac.com>
2011-10-03 11:36:07 -0700

    Initial Commit

---
+++ Test/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Test
+//
+//  Created by Jonathan Wight on 10/3/11.
+//  Copyright (c) 2011 toxicsoftware.com. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main (int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
