b0ffbcb83c45eb2cf551b9ad9fb681366bd8b389
Jonathan Wight <jwight@mac.com>
2011-10-04 14:40:01 -0700

    Added licenses.

--- CPropertyFactory.m
+++ CPropertyFactory.m
@@ -3,12 +3,41 @@
 //  Test
 //
 //  Created by Jonathan Wight on 10/3/11.
-//  Copyright (c) 2011 toxicsoftware.com. All rights reserved.
+//  Copyright 2011 Jonathan Wight. All rights reserved.
 //
+//  Redistribution and use in source and binary forms, with or without modification, are
+//  permitted provided that the following conditions are met:
+//
+//     1. Redistributions of source code must retain the above copyright notice, this list of
+//        conditions and the following disclaimer.
+//
+//     2. Redistributions in binary form must reproduce the above copyright notice, this list
+//        of conditions and the following disclaimer in the documentation and/or other materials
+//        provided with the distribution.
+//
+//  THIS SOFTWARE IS PROVIDED BY 2011 TOXICSOFTWARE.COM ``AS IS'' AND ANY EXPRESS OR IMPLIED
+//  WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+//  FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL 2011 TOXICSOFTWARE.COM OR
+//  CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+//  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+//  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+//  ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+//  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+//  ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+//  The views and conclusions contained in the software and documentation are those of the
+//  authors and should not be interpreted as representing official policies, either expressed
+//  or implied, of 2011 toxicsoftware.com.
 
 #import "CPropertyFactory.h"
 
 #import <objc/runtime.h>
+
+#define SIMPLE_GETTER(T, T2) ^(id _self) { return([inGetter(_self, thePropertyName) T##Value]); };
+#define SIMPLE_SETTER(T, T2) ^(id _self, T value) { inSetter(_self, thePropertyName, [NSNumber numberWith##T2:value]); };
+
+#define STRUCT_GETTER(T) ^(id _self) { T value; [inGetter(_self, thePropertyName) getValue:&value]; return(value); };
+#define STRUCT_SETTER(T) ^(id _self, T value) { inSetter(_self, thePropertyName, [NSValue valueWithBytes:&value objCType:@encode(T)]); };
 
 @interface CPropertyFactory ()
 @end
@@ -111,23 +140,23 @@
         {
         if (strcmp(theType, @encode(short)) == 0)
             {
-            theGetterIMPBlock = ^(id _self) { return([inGetter(_self, thePropertyName) shortValue]); };
-            theSetterIMPBlock = ^(id _self, short value) { inSetter(_self, thePropertyName, [NSNumber numberWithShort:value]); };
+            theGetterIMPBlock = SIMPLE_GETTER(short, Short);
+            theSetterIMPBlock = SIMPLE_SETTER(short, Short);
             }
-        if (strcmp(theType, @encode(int)) == 0)
+        else if (strcmp(theType, @encode(int)) == 0)
             {
-            theGetterIMPBlock = ^(id _self) { return([inGetter(_self, thePropertyName) intValue]); };
-            theSetterIMPBlock = ^(id _self, int value) { inSetter(_self, thePropertyName, [NSNumber numberWithInt:value]); };
+            theGetterIMPBlock = SIMPLE_GETTER(int, Int);
+            theSetterIMPBlock = SIMPLE_SETTER(int, Int);
             }
         else if (strcmp(theType, @encode(float)) == 0)
             {
-            theGetterIMPBlock = ^(id _self) { return([inGetter(_self, thePropertyName) floatValue]); };
-            theSetterIMPBlock = ^(id _self, float value) { inSetter(_self, thePropertyName, [NSNumber numberWithFloat:value]); };
+            theGetterIMPBlock = SIMPLE_GETTER(float, Float);
+            theSetterIMPBlock = SIMPLE_SETTER(float, Float);
             }
         else if (strcmp(theType, @encode(double)) == 0)
             {
-            theGetterIMPBlock = ^(id _self) { return([inGetter(_self, thePropertyName) doubleValue]); };
-            theSetterIMPBlock = ^(id _self, double value) { inSetter(_self, thePropertyName, [NSNumber numberWithDouble:value]); };
+            theGetterIMPBlock = SIMPLE_GETTER(double, Double);
+            theSetterIMPBlock = SIMPLE_SETTER(double, Double);
             }
         }
     else if (theType[0] == '{')
@@ -135,8 +164,8 @@
         #if TARGET_OS_IPHONE == 0
         if (strcmp(theType, @encode(NSPoint)) == 0)
             {
-            theGetterIMPBlock = ^(id _self) { NSPoint value; [inGetter(_self, thePropertyName) getValue:&value]; return(value); };
-            theSetterIMPBlock = ^(id _self, NSPoint value) { inSetter(_self, thePropertyName, [NSValue valueWithBytes:&value objCType:@encode(NSPoint)]); };
+            theGetterIMPBlock = STRUCT_GETTER(NSPoint);
+            theSetterIMPBlock = STRUCT_SETTER(NSPoint);
             }
         #endif
         }
